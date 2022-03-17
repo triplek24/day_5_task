a1e11d8849189c352fa376693e09f35c7383256a
Karthikeyan K <80576697+triplek24@users.noreply.github.com>
2022-03-17 14:03:06 +0530

    output is given as comments

--- arrow.js
+++ arrow.js
@@ -1,7 +1,6 @@
 //Task are done by the Arrow functions
 //Print odd number in an array 
-         
-            const num=[1,8,88,77,25,99,445];
+     const num=[1,8,88,77,25,99,445];
 
             console.log(num.filter((val)=>(val%2)!==0))
              //output [1,77,25,99,445]
@@ -39,3 +38,19 @@ console.log(myPrimeArray);
 		var palindromes = pal.filter((val)=>val.split("").reverse().join("")===val)
 
 		console.log(palindromes);
+		/*expected ouput
+		print odd number : [ 1, 77, 25, 99, 445 ]
+		
+		Convert all the strings to title caps in a string array:
+		[ 'LIFE', 'IS', 'A', 'CIRCLE', 'WIN', 'GUY', 'LOSE', 'LOSE GUY WIN' ]
+		
+		Sum of all numbers in an array: 1172
+		 
+		Return all the prime numbers in an array: [ 3, 2, 5 ]
+		
+		Return all the palindromes in an array :[]
+		*/
+		
+		
+		
+		
